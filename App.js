061a0ee26d6e221aea2335dca25e81098028175a
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-20 15:56:57 -0800

    An App that repeats the buttons in spanish sounds for the user upon clicking.
    A sound app for spanish learning of number 1-10 with playable sound.

--- App.js
+++ App.js
@@ -1,21 +1,256 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView
+} from "react-native";
+import { Audio } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const listBackgroundColors = {
+  1: "#3498DB",
+  2: "#E8290B",
+  3: "#AE1438",
+  4: "#2475B0",
+  5: "#30336B",
+  6: "#0A3D62",
+  7: "#26ae60",
+  8: "#10A881",
+  9: "#218F76",
+  10: "#DFAF2B"
+};
+
+const soundList = {
+  one: require("./assets/one.wav"),
+  two: require("./assets/two.wav"),
+  three: require("./assets/three.wav"),
+  four: require("./assets/four.wav"),
+  five: require("./assets/five.wav"),
+  six: require("./assets/six.wav"),
+  seven: require("./assets/seven.wav"),
+  eight: require("./assets/eight.wav"),
+  nine: require("./assets/nine.wav"),
+  ten: require("./assets/ten.wav")
+};
+
+
+export default class App extends React.Component {
+
+  playSound = async number => {
+    const soundObject = new Audio.Sound();
+    try {
+      let path = soundList[number];
+      await soundObject.loadAsync(path);
+      await soundObject
+        .playAsync()
+        .then(async playbackStatus => {
+          setTimeout(() => {
+            soundObject.unloadAsync();
+          }, playbackStatus.playableDurationMillis);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.gridContainer}>
+          <Image 
+            style={styles.logo}
+            source={require('./assets/logo.png')}
+          />
+          <View style={styles.rowContainer}>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[1] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('one')
+            }}        
+            >
+              <Text style={styles.itemText}>One</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[2] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('two')
+            }}        
+            >
+              <Text style={styles.itemText}>Two</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[3] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('three')
+            }}        
+            >
+              <Text style={styles.itemText}>Three</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[4] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('four')
+            }}        
+            >
+              <Text style={styles.itemText}>Four</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[5] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('five')
+            }}        
+            >
+              <Text style={styles.itemText}>Five</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[6] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('six')
+            }}        
+            >
+              <Text style={styles.itemText}>Six</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[7] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('seven')
+            }}        
+            >
+              <Text style={styles.itemText}>Seven</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[8] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('eight')
+            }}        
+            >
+              <Text style={styles.itemText}>Eight</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[9] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('nine')
+            }}        
+            >
+              <Text style={styles.itemText}>Nine</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            
+            style={[
+              { backgroundColor: listBackgroundColors[10] 
+              }, 
+              styles.item
+            ]}    
+            onPress={() => {
+              this.playSound('ten')
+            }}        
+            >
+              <Text style={styles.itemText}>Ten</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Image 
+            style={styles.logo}
+            source={require('./assets/logo.png')}
+          />
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  gridContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 5
   },
+  logo: {
+    height: 50,
+    width: 200,
+    resizeMode: 'contain',
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 50
+  },
+  rowContainer: {
+    flexDirection: 'row',
+  },
+  item: {
+    borderRadius: 20,
+    flex: 1,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2
+  },
+  itemText: {
+    color: '#fff',
+    fontFamily: 'Times New Roman',
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
 });
